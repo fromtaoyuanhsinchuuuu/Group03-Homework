27a94a1501ed54f1ca7144c4ee8454d3f0d95ebc
higobear <leoyeesir@gmail.com>
2024-11-23 00:27:19 +0800

    add subtasks.py

---
+++ subtasks.py
@@ -0,0 +1,14 @@
+[
+[0, ['0.in', '0.out', 1, 64 << 20, 64 << 20, ['block_0_1.txt', 'block_0_2.txt', 'block_0_3.txt']]],
+[0, ['1.in', '1.out', 1, 64 << 20, 64 << 20, ['block_1_1.txt', 'block_1_2.txt', 'block_1_3.txt']]],
+[10, ['10.in', '10.out', 1, 64 << 20, 64 << 20, ['block_10_1.txt', 'block_10_10.txt', 'block_10_11.txt', 'block_10_12.txt', 'block_10_13.txt', 'block_10_14.txt', 'block_10_15.txt', 'block_10_16.txt', 'block_10_17.txt', 'block_10_18.txt', 'block_10_19.txt', 'block_10_2.txt', 'block_10_20.txt', 'block_10_3.txt', 'block_10_4.txt', 'block_10_5.txt', 'block_10_6.txt', 'block_10_7.txt', 'block_10_8.txt', 'block_10_9.txt']]],
+[10, ['11.in', '11.out', 1, 64 << 20, 64 << 20, ['block_11_1.txt', 'block_11_10.txt', 'block_11_11.txt', 'block_11_12.txt', 'block_11_13.txt', 'block_11_14.txt', 'block_11_15.txt', 'block_11_16.txt', 'block_11_17.txt', 'block_11_18.txt', 'block_11_19.txt', 'block_11_2.txt', 'block_11_20.txt', 'block_11_3.txt', 'block_11_4.txt', 'block_11_5.txt', 'block_11_6.txt', 'block_11_7.txt', 'block_11_8.txt', 'block_11_9.txt']]],
+[10, ['2.in', '2.out', 1, 64 << 20, 64 << 20, ['block_2_1.txt', 'block_2_2.txt', 'block_2_3.txt']]],
+[10, ['3.in', '3.out', 1, 64 << 20, 64 << 20, ['block_3_1.txt', 'block_3_2.txt', 'block_3_3.txt', 'block_3_4.txt', 'block_3_5.txt']]],
+[10, ['4.in', '4.out', 1, 64 << 20, 64 << 20, ['block_4_1.txt', 'block_4_2.txt', 'block_4_3.txt', 'block_4_4.txt', 'block_4_5.txt', 'block_4_6.txt', 'block_4_7.txt', 'block_4_8.txt']]],
+[10, ['5.in', '5.out', 1, 64 << 20, 64 << 20, ['block_5_1.txt', 'block_5_10.txt', 'block_5_2.txt', 'block_5_3.txt', 'block_5_4.txt', 'block_5_5.txt', 'block_5_6.txt', 'block_5_7.txt', 'block_5_8.txt', 'block_5_9.txt']]],
+[10, ['6.in', '6.out', 1, 64 << 20, 64 << 20, ['block_6_1.txt', 'block_6_10.txt', 'block_6_11.txt', 'block_6_12.txt', 'block_6_2.txt', 'block_6_3.txt', 'block_6_4.txt', 'block_6_5.txt', 'block_6_6.txt', 'block_6_7.txt', 'block_6_8.txt', 'block_6_9.txt']]],
+[10, ['7.in', '7.out', 1, 64 << 20, 64 << 20, ['block_7_1.txt', 'block_7_10.txt', 'block_7_11.txt', 'block_7_12.txt', 'block_7_13.txt', 'block_7_14.txt', 'block_7_2.txt', 'block_7_3.txt', 'block_7_4.txt', 'block_7_5.txt', 'block_7_6.txt', 'block_7_7.txt', 'block_7_8.txt', 'block_7_9.txt']]],
+[10, ['8.in', '8.out', 1, 64 << 20, 64 << 20, ['block_8_1.txt', 'block_8_10.txt', 'block_8_11.txt', 'block_8_12.txt', 'block_8_13.txt', 'block_8_14.txt', 'block_8_15.txt', 'block_8_16.txt', 'block_8_2.txt', 'block_8_3.txt', 'block_8_4.txt', 'block_8_5.txt', 'block_8_6.txt', 'block_8_7.txt', 'block_8_8.txt', 'block_8_9.txt']]],
+[10, ['9.in', '9.out', 1, 64 << 20, 64 << 20, ['block_9_1.txt', 'block_9_10.txt', 'block_9_11.txt', 'block_9_12.txt', 'block_9_13.txt', 'block_9_14.txt', 'block_9_15.txt', 'block_9_16.txt', 'block_9_17.txt', 'block_9_18.txt', 'block_9_2.txt', 'block_9_3.txt', 'block_9_4.txt', 'block_9_5.txt', 'block_9_6.txt', 'block_9_7.txt', 'block_9_8.txt', 'block_9_9.txt']]]
+]
